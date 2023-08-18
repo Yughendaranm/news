28d0ec3a9e11c9a4e55d1f35f82be405cc46d8fd
Yughendaranm <92256197+Yughendaranm@users.noreply.github.com>
2023-08-18 10:52:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+//API Used: http://newsapi.org/s/india-news-api
+const container = document.querySelector(".container");
+const optionsContainer = document.querySelector(".options-container");
+// "in" stands for India
+const country = "in";
+const options = [
+  "general",
+  "health",
+  "technology",
+  "education",
+  "trend"
+];
+
+//100 requests per day
+let requestURL;
+
+//Create cards from data
+const generateUI = (articles) => {
+  for (let item of articles) {
+    // console.log(item.image)
+    if(item.image != "None"){
+    let card = document.createElement("div");
+    card.classList.add("news-card");
+    card.innerHTML = `<div class="news-image-container">
+    <img src="${item.image || "./newspaper.jpg"}" alt="" />
+    </div>
+    <div class="news-content">
+      <div class="news-title">
+        ${item.title}
+      </div>
+      <div class="news-description">
+      ${item.description || item.content || ""}
+      </div>
+      <a href="${item.url}" target="_blank" class="view-button">Read More</a>
+    </div>`;
+    container.appendChild(card);
+    }
+  }
+};
+
+//News API Call
+const getNews = async () => {
+  container.innerHTML = "";
+  let response = await fetch(requestURL);
+  if (!response.ok) {
+    alert("Data unavailable at the moment. Please try again later");
+    return false;
+  }
+  let data = await response.json();
+  generateUI(data.news);
+};
+
+//Category Selection
+const selectCategory = (e, category) => {
+  let options = document.querySelectorAll(".option");
+  options.forEach((element) => {
+    element.classList.remove("active");
+  });
+  requestURL = 'https://api.currentsapi.services/v1/search?' +
+  `keywords=${category.toLowerCase()}&language=en&` + 
+  'apiKey=bUUoF0zmSdL1S7UfcvspulmRNOjvPFHzByMWFrX5CPvK74Qh';
+  e.target.classList.add("active");
+  // console.log(requestURL);
+  getNews();
+};
+
+//Options Buttons
+const createOptions = () => {
+  for (let i of options) {
+    optionsContainer.innerHTML += `<button class="option ${
+      i == "general" ? "active" : ""
+    }" onclick="selectCategory(event,'${i}')">${i}</button>`;
+  }
+};
+
+const init = () => {
+  optionsContainer.innerHTML = "";
+  getNews();
+  createOptions();
+};
+
+window.onload = async () => {
+  requestURL = 'https://api.currentsapi.services/v1/search?' +
+  'apiKey=bUUoF0zmSdL1S7UfcvspulmRNOjvPFHzByMWFrX5CPvK74Qh';
+  init();
+};
